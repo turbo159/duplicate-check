d12b369af1ce93c6680c2061aec05d7f6c8bd912
turbo159 <turbo159@outlook.com>
2020-01-24 20:19:19 -0600

    Test flags

--- cmd/dupe-check/dupe-check.go
+++ cmd/dupe-check/dupe-check.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("hello world")
+	argConfig := flag.String("config", "./boomi_automate.yml", "Path to config file")
+	argSecret := flag.String("secret", "", "Path to config file")
+	flag.Parse()
+	fmt.Println("Config:", *argConfig)
+	fmt.Println("Secret:", *argSecret)
 }
